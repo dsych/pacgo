652426d819923793b4a85617a9f161a900786f8e
Dmytro Sych <dmytro.sych@cgi.com>
2019-07-26 14:14:39 -0400

    finished step 2

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 )
@@ -48,12 +49,58 @@ func globalizeLevels(levels []string) error {
 }
 
 func printMaze(maze []string) {
+	clearScreen()
 	for _, row := range maze {
 		fmt.Println(row)
 	}
 }
 
+func init() {
+	cbTerm := exec.Command("/bin/stty", "cbreak", "-echo")
+	cbTerm.Stdin = os.Stdin
+
+	err := cbTerm.Run()
+
+	if err != nil {
+		log.Fatalln("Unable to activate cbreak mode terminal: %v\n", err)
+	}
+}
+
+func cleanup() {
+	cookedTerm := exec.Command("/bin/stty", "-cbreak", "echo")
+	cookedTerm.Stdin = os.Stdin
+
+	err := cookedTerm.Run()
+	if err != nil {
+		log.Fatalln("Unable to enable cooked mode: %v", err)
+	}
+}
+
+func readInput() (string, error) {
+	buffer := make([]byte, 100)
+
+	cnt, err := os.Stdin.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	if cnt == 1 && buffer[0] == 0x1b {
+		return "ESC", nil
+	}
+	return "", nil
+}
+
+func clearScreen() {
+	fmt.Print("\x1b[2J")
+	moveCursor(0, 0)
+}
+
+func moveCursor(row, col int) {
+	fmt.Printf("\x1b[%d;%df", row+1, col+1)
+}
+
 func main() {
+	defer cleanup()
 	// initialize game
 	levels := []string{filepath.Join("step01", "maze01.txt")}
 	err := globalizeLevels(levels)
@@ -71,7 +118,13 @@ func main() {
 		// update screen
 		printMaze(maze)
 		// process input
-
+		input, err := readInput()
+		if err != nil {
+			log.Fatalln("Unable to process keyboard input:\n%v", err)
+		}
+		if input == "ESC" {
+			break
+		}
 		// process movement
 
 		// process collisions
@@ -79,8 +132,6 @@ func main() {
 		// check game over
 
 		// Temp: break infinite loop
-		fmt.Println("Hello, Pac Go!")
-		break
 
 		// repeat
 	}
